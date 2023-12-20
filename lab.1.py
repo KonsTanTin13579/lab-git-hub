eac06eee532ad8abf456c1dca212aba022db4a78
KonsTanTin13579 <grond1543@gmail.com>
2023-12-20 19:28:50 +0700

    new file:   lab.1.py

---
+++ lab.1.py
@@ -0,0 +1,22 @@
+n = int(input())
+s = 0 
+otvet = 1
+for i in range (1 , n + 1 , 2) :
+    s += i 
+print("сумма:", s )
+print(otvet)
+ 
+#n = int(input())
+#mn = 10**9
+#for i in range (1 , n +1 ):
+#    mn = min(i**2 - 15*i, mn)
+#print(mn)
+
+#n = int(input())
+#d = 0
+#for i in range (1 , n+1 ):
+#    d =  7*i - 5
+#    print(i, d)
+#    if d % 3 == 0 :
+#    print(d)
+
